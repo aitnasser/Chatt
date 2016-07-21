7802cceb51229a2e29dd0968c702ef96efa4dac1
pandabr <guh.clemente@gmail.com>
2016-07-21 10:52:26 -0300

    Initial commit

---
+++ app/src/main/java/br/com/wfstudio/chatt/util/Constants.java
@@ -0,0 +1,33 @@
+package br.com.wfstudio.chatt.util;
+
+/**
+ * Created by GleasonK on 7/30/15.
+ */
+public class Constants {
+    public static final String SHARED_PREFS = "br.com.wfstudio.chatt.SHARED_PREFS";
+    public static final String USER_NAME    = "br.com.wfstudio.chatt.SHARED_PREFS.USER_NAME";
+    public static final String CALL_USER    = "br.com.wfstudio.chatt.SHARED_PREFS.CALL_USER";
+    public static final String STDBY_SUFFIX = "-stdby";
+
+    public static final String PUB_KEY = "pub-c-fc75f345-7311-4ae1-a9b4-a06ac47968d8"; // Your Pub Key
+    public static final String SUB_KEY = "sub-c-8265c8fa-4f3b-11e6-8b3b-02ee2ddab7fe"; // Your Sub Key
+
+//    public static final String PUB_KEY = "demo"; // Your Pub Key
+//    public static final String SUB_KEY = "demo"; // Your Sub Key
+
+    public static final String JSON_CALL_USER = "call_user";
+    public static final String JSON_CALL_TIME = "call_time";
+    public static final String JSON_OCCUPANCY = "occupancy";
+    public static final String JSON_STATUS    = "status";
+
+    // JSON for user messages
+    public static final String JSON_USER_MSG  = "user_message";
+    public static final String JSON_MSG_UUID  = "msg_uuid";
+    public static final String JSON_MSG       = "msg_message";
+    public static final String JSON_TIME      = "msg_timestamp";
+
+    public static final String STATUS_AVAILABLE = "Available";
+    public static final String STATUS_OFFLINE   = "Offline";
+    public static final String STATUS_BUSY      = "Busy";
+
+}
